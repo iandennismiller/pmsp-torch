c43acc15e68a1aaa0cf00d2bc718fcacc77d658f
Ian Dennis Miller <ian@iandennismiller.com>
2020-10-03 10:10:41 -0400

    individual experiments in separate files

--- bin/pmsp.py
+++ bin/pmsp.py
@@ -12,33 +12,11 @@ from torch.utils.data import DataLoader
 import sys
 sys.path.insert(0, '.')
 
-from pmsp.stimuli import PMSPStimuli
-from pmsp.dataset import PMSPDataset
-from pmsp.device_dataloader import DeviceDataLoader
-from pmsp.network import PMSPNetwork
-from pmsp.trainer import PMSPTrainer
-from pmsp.simulator import Simulator
 
 @click.group()
 def cli():
     pass
 
-@click.command('test', short_help='Just test whether it runs.')
-def just_test():
-    stimuli = PMSPStimuli(
-        mapping_filename="PMSP/data/plaut_dataset_collapsed.csv",
-        frequency_filename="PMSP/data/word-frequencies.csv",
-        )
-    dataset = PMSPDataset(stimuli.df)
-    network = PMSPNetwork(dataset=dataset)
-
-    # stimuli = PMSPStimuli()
-    # result = stimuli.generate_stimuli_log_transform(percentage=0.95)
-    # assert(result)
-
-    sim = Simulator(model=network)
-    summary(sim.model, input_size=(1, 1, sim.model.input_size))
-    sim.go(num_epochs=3)
 
 @click.command('generate', short_help='Generate data.')
 @click.option('--infile', required=True, help='File to read from.')
@@ -49,33 +27,15 @@ def generate(infile, outfile):
     with open(outfile, 'w') as f:
         f.write(result)
 
-@click.command('simulate', short_help='Run simulation training.')
-@click.option('--rate', default=0.001, help='Learning rate.')
-@click.option('--epochs', default=300, help='Number of epochs.')
-def simulate(rate, epochs):
-    mapping_filename = "PMSP/data/plaut_dataset_collapsed.csv"
-    frequency_filename = "PMSP/data/word-frequencies.csv"
 
-    stimuli = PMSPStimuli(
-        mapping_filename=mapping_filename,
-        frequency_filename=frequency_filename
-    )
-    dataset = PMSPDataset(stimuli.df)
-    dataloader = DeviceDataLoader(DataLoader(
-        dataset,
-        batch_size=len(dataset),
-        num_workers=0
-    ))
+@click.command('inspect-vowel-activation')
+def inspect_vowel_activation():
+    from pmsp.experiments.inspect_vowel_activation import go
+    go()
 
-    network = PMSPNetwork()
-    trainer = PMSPTrainer(network=network, dataloader=dataloader)
-
-    sim = Simulator()
-    sim.go(trainer, num_epochs=epochs)
 
 cli.add_command(generate)
-cli.add_command(just_test)
-cli.add_command(simulate)
+cli.add_command(inspect_vowel_activation)
 
 if __name__ == '__main__':
     if not os.path.isdir('var'):
@@ -86,6 +46,6 @@ if __name__ == '__main__':
         level=logging.INFO
     )
 
-    from PMSP.__meta__ import __version__
+    from pmsp.__meta__ import __version__
     print("pmsp-torch", __version__)
     cli()
---
+++ pmsp/experiments/inspect_vowel_activation.py
@@ -0,0 +1,47 @@
+import os
+import click
+import logging
+import torch
+from torchsummary import summary
+from torch.utils.data import DataLoader
+
+import sys
+sys.path.insert(0, '.')
+
+from pmsp.stimuli import PMSPStimuli
+from pmsp.dataset import PMSPDataset
+from pmsp.device_dataloader import DeviceDataLoader
+from pmsp.network import PMSPNetwork
+from pmsp.trainer import PMSPTrainer
+
+
+def go():
+    torch.manual_seed(1)
+
+    # stimuli are drawn from these CSV files
+    stimuli = PMSPStimuli(
+        mapping_filename="pmsp/data/plaut_dataset_collapsed.csv",
+        frequency_filename="pmsp/data/word-frequencies.csv"
+    )
+
+    # build dataset from stimuli
+    dataset = PMSPDataset(stimuli.df)
+
+    # build dataloader from dataset
+    dataloader = DeviceDataLoader(DataLoader(
+        dataset,
+        batch_size=len(dataset),
+        num_workers=0
+    ))
+
+    # set up the network
+    network = PMSPNetwork()
+    summary(network, input_size=(1, 1, network.input_size))
+
+    # set up the PMSP trainer to use this network and dataloader
+    trainer = PMSPTrainer(network=network, dataloader=dataloader)
+
+    # run for 350 epochs
+    trainer.train(num_epochs=350)
+
+    # print(network)
