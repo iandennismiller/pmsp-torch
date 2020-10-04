0a65c7f2539a410ff4c3fbe625586f50a277abb1
Ian Dennis Miller <ian@iandennismiller.com>
2020-10-04 17:03:06 -0400

    remove reference calculate accuracy

--- src/pmsp/util/__init__.py
+++ src/pmsp/util/__init__.py
@@ -64,31 +64,3 @@ def make_folder():
             i += 1
 
     return(rootdir)
-
-def calculate_accuracy(data):
-    # https://github.com/Brian0615/Plaut_Model_v2/blob/0b3009b20ed366f6bdc9bd262c7bdd7f3869f33f/simulator/simulator.py#L434
-
-    # accuracy calculations - accuracy is based on highest activity vowel phoneme
-
-    inputs = data['graphemes']
-    targets = data['phonemes'].clone()
-
-    outputs_max_vowel = outputs[:, 23:37].argmax(dim=1)
-    targets_max_vowel = targets[:, 23:37].argmax(dim=1)
-    compare = torch.eq(outputs_max_vowel, targets_max_vowel).tolist()
-
-    correct, total = [], []
-    # find correct and total for each category
-    for cat in categories:
-        if cat == 'All':  # find accuracy across all categories
-            correct.append(sum(compare) / len(data['type']))
-            total.append(len(data['type']))
-        else:
-            temp_total, temp_correct = 0, 0
-            for t, c in zip(data['type'], compare):
-                if t == cat:  # if same category
-                    temp_total += 1
-                    if c:  # if correct
-                        temp_correct += 1
-            total.append(temp_total)
-            correct.append(temp_correct)
